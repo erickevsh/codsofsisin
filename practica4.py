73207cee38d46c70ab738faeab7e4d4e2f3dbe79
erickevsh <eric.flores.2024@cbtis-246.edu.mx>
2025-03-03 01:33:29 -0600

    Add files via upload

---
+++ practica4.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Solicitar información al usuario
+nombre = input("Ernesto ")
+edad = input("15 ")
+profesion = input("diseñador ")
+
+# Imprimir el texto con los datos ingresados
+print("\nMi nombre es", ernesto, "y tengo", 15, "años de edad.")
+print("También, siempre he querido ser", diseñador + ".")
+print("¡Gracias por leerme!")
+
+
+# In[ ]:
+
+
+
+
