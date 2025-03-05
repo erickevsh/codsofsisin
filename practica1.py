ef8c05c14457e1f605ee7e4ad7bb40fdbe7296fb
erickevsh <eric.flores.2024@cbtis-246.edu.mx>
2025-03-04 19:56:14 -0600

    Add files via upload

---
+++ practica1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+enero = 31
+febrero = 28 
+marzo = 31
+abril = 30 
+mayo = 31 
+junio = 30 
+julio = 31
+agosto = 31 
+setptiembre = 30
+octubre = 31
+noviembre = 30
+diciembre = 31
+
+dia_nac = 7
+mes_nac = 4
+año_nac = 2009
+
+dia_hoy = 4
+mes_hoy = 3
+año_actual = 2025
+
+print(2025)
+
+
+# In[ ]:
+
+
+
+
